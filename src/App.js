7e348438e3d7a96378828ee53f46b567223402bb
Shawn Cochran <shawn.cochran31@gmail.com>
2022-01-14 16:48:00 -0700

    removed extras

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
 } from '@chakra-ui/react';
@@ -20,18 +17,6 @@ function App() {
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
           </VStack>
         </Grid>
       </Box>
